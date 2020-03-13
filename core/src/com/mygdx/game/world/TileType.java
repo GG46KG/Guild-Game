2fd2788862e0dbcff658fd51ed90e63168f0ea34
GG46KG <54337018+GG46KG@users.noreply.github.com>
2020-03-12 22:52:19 -0500

    Added new tiletypes

--- core/src/com/mygdx/game/world/TileType.java
+++ core/src/com/mygdx/game/world/TileType.java
@@ -5,7 +5,12 @@ import com.badlogic.gdx.graphics.Color;
 
 public enum TileType {
     FOREST ("Forest", new Color(.34f, .49f, .192f, 1)),
-    MOUNTAIN ("mountain", new Color(.686f, .58f, .364f,1));
+    MOUNTAIN ("mountain", new Color(.686f, .58f, .364f,1)),
+    WATER ("Waves", new Color(0x113ea7ff)),
+    DESERT ("Waves", new Color(0xe5dc8aff)),
+    PEAK ("Peak", new Color(0xffffffff)),
+    VOLCANO("Peak", new Color(0x591212ff)),
+    PLAINS ("GentleHills", new Color(0xa6d22dff));
 
     public final String spriteName;
     public final Color spriteColor;
